0280bd9f6bd38e6d83ab3c223a7605a6c0e226ab
Kengo Nakajima <kengo.nakajima@gmail.com>
2017-06-03 07:14:51 +0900

    02x

--- mcupdate.rb
+++ mcupdate.rb
@@ -11,12 +11,14 @@ hash=JSON.parse(json)
 
 svt = hash["servertime"].to_i
 sec = (svt * 3.6).to_i
-min = (sec/60).to_i
-hour = (min/60).to_i
+totalmin = (sec/60).to_i
+min = totalmin%60
+hour = (totalmin/60).to_i
 
 num = hash["currentcount"].to_i
 
-print "#{hour}:#{min%60}(#{num})"
+printf( "%02d:%02d(%d)", hour,min,num)
+
 
 
 
