03714887e4674c621f0dc0105bc43964cffe991c
Kengo Nakajima <kengo.nakajima@gmail.com>
2018-01-15 11:35:20 +0900

    ccu

--- 256tsv.rb
+++ 256tsv.rb
@@ -7,8 +7,13 @@ require "pp"
 apikey=File.open("/Users/ringo/doc/keys/mackerel.factorio.api.key").read.strip
 hostid="3chCRGrPtsG"
 json = `curl  --silent -H 'Content-Type: application/json' -H 'X-Api-Key: #{apikey}' https://mackerel.io/api/v0/hosts/#{hostid}`
-h = JSON.parse(json) 
-print h["host"]["meta"]["filesystem"]["/dev/root"]["percent_used"]
+h = JSON.parse(json)
+
+# CCU
+json=`curl --silent http://256t.test-tube.net/stats`
+stath=JSON.parse(json)
+ccu=stath["ccu"]
+print h["host"]["meta"]["filesystem"]["/dev/root"]["percent_used"] + "(#{ccu})"
 
 
 
